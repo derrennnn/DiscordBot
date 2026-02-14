26a3bcd5231d0afc26f4a121f71ef6687e88bac5
derrennnn <devans30105@gmail.com>
2026-02-14 04:13:47 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,14 @@
  * Version 2.1.7
  * Robert Borghese
  ******************************************************/
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Bot is running"));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server started");
+});
 
 const DBM = {};
 DBM.version = "2.1.7";
